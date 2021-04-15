947555282a6c915325cad8c5a090d17f83b70bef
Ramganesh <ramganesh@animaker.com>
2021-04-15 19:16:43 +0530

    Add init files

---
+++ preload.js
@@ -0,0 +1,15 @@
+window.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        const replaceText = (selector, text) => {
+            const element = document.getElementById(selector)
+            if (element) {
+                element.innerText = text
+            }
+        }
+
+        for (const type of ['node', 'chrome', 'electron']) {
+            replaceText(`${type}-version`, process.versions[type])
+        }
+    }
+)
