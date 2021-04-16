81e1f6b31fc46850f755c0d0ab4f4434500fa8b2
Ramganesh <ramganesh@animaker.com>
2021-04-16 17:42:19 +0530

    Add tray and show application on tray click

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { BrowserWindow, app } = require("electron");
+const { Tray } = require("electron/main");
 const path = require('path')
 
 let mainWindow
+let tray
 function createWindow() {
     mainWindow = new BrowserWindow(
         {
-            width: 800,
-            height: 600,
+            width: 300,
+            height: 450,
+            frame: false,
+            resizable: false,
+            show: false,
             webPreferences: {
                 preload: path.join(__dirname, 'preload.js')
             }
@@ -14,6 +19,32 @@ function createWindow() {
     )
 
     mainWindow.loadFile('index.html')
+
+    const icon = process.platform === 'win32' ? 'icon-win.png' : 'icon.png'
+
+    tray = new Tray(
+        path.join(__dirname, 'assets', icon)
+    )
+
+    tray.on('click', (event, bounds) => {
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            const { x, y } = bounds
+            const { width, height } = mainWindow.getBounds()
+
+            const yOffset = process.platform === 'darwin' ? y : y - height
+            mainWindow.setBounds(
+                {
+                    width,
+                    height,
+                    x: x - width / 2,
+                    y: yOffset
+                }
+            )
+            mainWindow.show()
+        }
+    })
 }
 
 app.whenReady()
