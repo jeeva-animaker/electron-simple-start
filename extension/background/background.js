425aaa08df04059acd89936c952a7c16f01ad169
Jeevanantham <jeevanantham@animaker.com>
2021-04-27 12:57:21 +0530

    Init

---
+++ extension/background/background.js
@@ -0,0 +1,80 @@
+console.log('background')
+
+let RECORDER = {
+    permission: false,
+    constraints: {
+        video: true
+    },
+    instance: null,
+    stream: null,
+    blobs: []
+}
+const sendMessageById = (id, msg) => {
+    chrome.tabs.sendMessage(id, msg);
+}
+const sendMessage = (msg) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTab = tabs[0];
+        if (activeTab) {
+            sendMessageById(activeTab.id, msg);
+        }
+    });
+}
+
+function getStream() {
+    return navigator.mediaDevices.getUserMedia(
+        RECORDER.constraints
+    )
+}
+
+chrome.runtime.onMessage.addListener(
+    (e) => {
+        switch (e.message) {
+            case 'video:permission:success':
+                RECORDER.permission = true
+                break;
+            case 'video:record:start':
+                startRecord()
+                break;
+            case 'video:record:stop':
+                stopRecord()
+                break;
+            default:
+                console.log(e.message)
+                break;
+        }
+    }
+)
+
+function startRecord() {
+    getStream()
+        .then(stream => {
+            RECORDER.stream = stream
+            RECORDER.instance = RecordRTC(
+                stream,
+                {
+                    type: 'video',
+                    mimeType: 'video/webm',
+                    recorderType: MediaStreamRecorder
+                }
+            )
+            RECORDER.instance.startRecording()
+            sendMessage(
+                {
+                    message: 'video:record:started'
+                }
+            )
+        })
+}
+
+function stopRecord() {
+    RECORDER.instance.stopRecording(blob => {
+        RECORDER.instance.save('video.webm')
+    })
+    RECORDER.stream && RECORDER.stream.stop()
+    sendMessage(
+        {
+            message: 'video:record:stopped'
+        }
+    )
+}
